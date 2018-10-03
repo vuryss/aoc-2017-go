2dbe078d6e5e73d064a225c4d23bc76d2dd61344
Georgi Georgiev <vuryss@gmail.com>
2018-10-03 18:47:51 +0300

    Day 10 completed

--- day-10.go
+++ day-10.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -74,8 +75,6 @@ func part1(input string) {
 		position += length + skipSize
 		position %= 256
 		skipSize++
-
-		//log.Print(list)
 	}
 
 	log.Printf("Value: %v", list[0] * list[1])
@@ -83,38 +82,91 @@ func part1(input string) {
 	log.Printf("Execution time: %v", time.Since(start))
 }
 
-//func part2(input string) {
-//	start := time.Now()
-//	inputLen := len(input)
-//	lastIndex := inputLen - 1
-//	offset := len(input) / 2
-//	sum := 0
-//
-//	for i := range input {
-//		nextI := i + offset
-//
-//		if nextI > lastIndex {
-//			nextI %= inputLen
-//		}
-//
-//		if input[i] == input[nextI] {
-//			digit, err := strconv.Atoi(string(input[i]))
-//
-//			if err != nil {
-//				log.Fatal(err)
-//				break
-//			}
-//
-//			sum += digit
-//		}
-//	}
-//
-//	log.Printf("Sum: %v", sum)
-//	log.Printf("Execution time: %v", time.Since(start))
-//}
+func part2(input string) {
+	start := time.Now()
+	lengths := []rune(input)
+	lengths = append(lengths, 17, 31, 73, 47, 23)
+	list := make([]int, 256)
+	position := 0
+	skipSize := 0
+
+	for i := range list {
+		list[i] = i
+	}
+
+	for round := 0; round < 64; round++ {
+		for i := range lengths {
+			length := int(lengths[i])
+			seq := make([]int, 0)
+
+			// To end of the list
+			end := position + length - 1
+			wrap := false
+
+			if end > 255 {
+				wrap = true
+				end = 255
+			}
+
+			for i := position; i <= end; i++ {
+				seq = append(seq, list[i])
+			}
+
+			// From the beginning
+			if wrap {
+				end = position + length - 256
+
+				for i := 0; i <= end; i++ {
+					seq = append(seq, list[i])
+				}
+			}
+
+			// Reverse sequence
+			halfLength := length / 2
+			for i := 0; i < halfLength; i++ {
+				seq[i], seq[length - 1 - i] = seq[length - 1 - i], seq[i]
+			}
+
+			// Save to list
+			if wrap {
+				for i := position; i < 256; i++ {
+					list[i], seq = seq[0], seq[1:]
+				}
+
+				for i := 0; i <= end; i++ {
+					list[i], seq = seq[0], seq[1:]
+				}
+			} else {
+				for i := position; i <= end; i++ {
+					list[i], seq = seq[0], seq[1:]
+				}
+			}
+
+			position += length + skipSize
+			position %= 256
+			skipSize++
+		}
+	}
+
+	denseHash := make([]int, 16)
+	hash := ""
+
+	for i := 0; i < 16; i++ {
+		for j := i * 16; j < (i + 1) * 16; j++ {
+			denseHash[i] ^= list[j]
+		}
+	}
+
+	for i := range denseHash {
+		hash += fmt.Sprintf("%02x", denseHash[i])
+	}
+
+	log.Printf("Dense hash: %v", hash)
+	log.Printf("Execution time: %v", time.Since(start))
+}
 
 func main() {
 	input := getInput("day-10")
 	part1(input)
-	//part2(input)
+	part2(input)
 }

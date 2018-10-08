ce4d456e6028af0751b4f4e02fc3e894f42d2d84
Georgi Georgiev <ggeorgiev@shutterfly.com>
2018-10-08 16:10:50 +0300

    Day 18 solved without deadlock error

--- day-18.go
+++ day-18.go
@@ -23,35 +23,35 @@ func day18part1(input string) {
 		case "snd":
 			lastFrequency, _ = registers[register]
 		case "set":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
 			registers[register] = intValue
 		case "add":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
 			registerValue, _ := registers[register]
 			registers[register] = registerValue + intValue
 		case "mul":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
 			registerValue, _ := registers[register]
 			registers[register] = registerValue * intValue
 		case "mod":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
@@ -65,9 +65,9 @@ func day18part1(input string) {
 				break EXEC
 			}
 		case "jgz":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
@@ -89,11 +89,17 @@ func day18part1(input string) {
 	log.Printf("Execution time: %v", time.Since(start))
 }
 
-func sound(instructions []string, id int, selfQueue chan int, otherQueue chan int, wg sync.WaitGroup) {
+func sound(instructions []string, id int, selfQueue chan int, otherQueue chan int, lock chan bool, wg *sync.WaitGroup) {
 	position := 0
 	registers := make(map[rune]int)
 	registers['p'] = id
 	sentCount := 0
+	defer func() {
+		if id == 1 {
+			log.Printf("Program 1 sent count: %v", sentCount)
+		}
+		wg.Done()
+	}()
 
 	for {
 		parts := strings.Fields(instructions[position])
@@ -104,51 +110,65 @@ func sound(instructions []string, id int, selfQueue chan int, otherQueue chan in
 			value, _ := registers[register]
 			otherQueue <- value
 			sentCount++
-
-			if id == 1 {
-				log.Printf("Program 1 sent %v times", sentCount)
-			}
 		case "set":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
 			registers[register] = intValue
 		case "add":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
 			registerValue, _ := registers[register]
 			registers[register] = registerValue + intValue
 		case "mul":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
 			registerValue, _ := registers[register]
 			registers[register] = registerValue * intValue
 		case "mod":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
 			registerValue, _ := registers[register]
 			registers[register] = registerValue % intValue
 		case "rcv":
-			registers[register] = <- selfQueue
+			isLocked := false
+			OUTER:
+			for {
+				select {
+				case registers[register] = <-selfQueue:
+					if isLocked {
+						<-lock
+					}
+					break OUTER
+				default:
+					if !isLocked {
+						lock <- true
+					}
+					isLocked = true
+					if len(lock) == 2 {
+						return
+					}
+				}
+			}
 		case "jgz":
-			intValue, error := strconv.Atoi(parts[2])
+			intValue, err := strconv.Atoi(parts[2])
 
-			if error != nil {
+			if err != nil {
 				intValue, _ = registers[[]rune(parts[2])[0]]
 			}
 
@@ -173,8 +193,6 @@ func sound(instructions []string, id int, selfQueue chan int, otherQueue chan in
 	}
 
 	log.Printf("Channel 1 sent %v times", sentCount)
-
-	defer wg.Done()
 }
 
 func day18part2(input string) {
@@ -184,10 +202,13 @@ func day18part2(input string) {
 
 	channel1 := make(chan int, 100000)
 	channel2 := make(chan int, 100000)
+	lock := make(chan bool, 2)
 	wg.Add(2)
-	go sound(lines, 0, channel1, channel2, wg)
-	go sound(lines, 1, channel2, channel1, wg)
+	go sound(lines, 0, channel1, channel2, lock, &wg)
+	go sound(lines, 1, channel2, channel1, lock, &wg)
 	wg.Wait()
+	//<-lock
+	//<-lock
 
 	log.Printf("Execution time: %v", time.Since(start))
 }
